e604931466de8dd5b954be9eff623b5896cca719
MyouriAlter <innocentbystander0123@gmail.com>
2021-01-25 17:37:30 +0700

    Updated Category Controller
    Just a minor update to Category and its Controller

--- Controllers/CategoryController.cs
+++ Controllers/CategoryController.cs
@@ -3,14 +3,30 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using ASP.NET_Core_MVC_Basic.Database;
+using ASP.NET_Core_MVC_Basic.Models;
 
 namespace ASP.NET_Core_MVC_Basic.Controllers
 {
     public class CategoryController : Controller
     {
-        public IActionResult Index()
+        private readonly ApplicationDdContext _applicationDdContext;
+
+        public CategoryController(ApplicationDdContext applicationDdContext)
+        {
+            _applicationDdContext = applicationDdContext;
+        }
+
+        public IActionResult Category()
+        {
+            IEnumerable<CategoryModel> category = _applicationDdContext.Category;
+            return View(category);
+        }
+
+        public IActionResult Create()
         {
             return View();
         }
+
     }
 }
